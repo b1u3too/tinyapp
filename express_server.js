bb763f978080ae31a9e7b6d569b2d8d3cb972dd0
b1u3too <almadsen792@live.ca>
2022-03-04 03:58:39 +0000

    write user input to urlDatabase

--- express_server.js
+++ express_server.js
@@ -45,8 +45,10 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body); //Log the POST request body to the Console
+  urlDatabase[generateRandomString()] = req.body.longURL; //save new shortURL/longURL pair to the database
+  console.log('urlDatabase', urlDatabase);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
