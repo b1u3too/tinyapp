ef856b05fe44ed8ccb37fd13bfa809df54a8e11a
b1u3too <almadsen792@live.ca>
2022-03-05 18:13:15 +0000

    add app.get for /u/:shortURL links

--- express_server.js
+++ express_server.js
@@ -44,6 +44,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); //Log the POST request body to the Console
   const shortURL = generateRandomString();
